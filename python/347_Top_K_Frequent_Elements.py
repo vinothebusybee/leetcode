8768ec81a5ef419308ebaee27090da4a7be07595
vinothebusybee <51785775+vinothebusybee@users.noreply.github.com>
2022-03-23 17:20:43 +0530

    Update 347_Top_K_Frequent_Elements.py

--- python/347_Top_K_Frequent_Elements.py
+++ python/347_Top_K_Frequent_Elements.py
@@ -8,3 +8,33 @@ class Solution(object):
         counter = collections.Counter(nums)
         return [k for k,v in counter.most_common(k)]
         # return heapq.nlargest(k, count.keys(), key=count.get)
+        
+    def vintopKFrequent(self, nums, k):
+        
+        counter = {}
+
+        for n in nums:
+            counter[n] = 1 + counter.get(n ,0)
+        
+        freq=[]
+        
+        for i in counter:
+            freq.append([i,counter[i]])
+        
+        freq.sort(key = lambda x: x[1],reverse=True)
+        
+        print(k,"numbers with most occurrences are")
+        
+        res=[]
+        
+        for i in range(k):
+            res.append(freq[i][0])
+            
+        return res
+        
+nums = [3, 1, 4, 4, 5, 2, 6, 1]
+k = 2
+#2 numbers with most occurrences are
+#1 4 
+
+
